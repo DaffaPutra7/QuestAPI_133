c4c9897b224a4b2eff8f224d82d35c79189afd00
DaffaPutra7 <putradika3604@gmail.com>
2024-12-29 14:29:05 +0700

    Membuat package baru didalam package ui yaitu package viewmodel setelah itu membuat file InsertViewModel yang didalamnya terdapat 3 fungsi

---
+++ app/src/main/java/com/example/pam_pertemuan11/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.pam_pertemuan11.ui.viewmodel
+
+import com.example.pam_pertemuan11.model.Mahasiswa
+
+class InsertViewModel {
+}
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
