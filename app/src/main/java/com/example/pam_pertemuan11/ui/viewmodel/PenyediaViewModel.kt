4306aa824df7d4c657ba9bd18e1cb563502abf88
DaffaPutra7 <putradika3604@gmail.com>
2024-12-29 17:04:53 +0700

    Membuat file PenyediaViewModel.kt di package viewmodel yang berisikan fungsi CreationExtras dan object PenyediaViewModel

---
+++ app/src/main/java/com/example/pam_pertemuan11/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.pam_pertemuan11.ui.viewmodel
+
+import android.text.Editable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pam_pertemuan11.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
