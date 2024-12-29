250f166ee47fedbe967f28a648582569540c85c8
DaffaPutra7 <putradika3604@gmail.com>
2024-12-29 14:49:43 +0700

    Menambah code pada file HomeViewModel.kt berupa class HomeViewModel yang didalamnya terdapat var dan beberapa fungsi

--- app/src/main/java/com/example/pam_pertemuan11/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pam_pertemuan11/ui/viewmodel/HomeViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.pam_pertemuan11.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.pam_pertemuan11.model.Mahasiswa
+import com.example.pam_pertemuan11.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import okio.IOException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
@@ -8,5 +17,36 @@ sealed class HomeUiState {
     object Loading : HomeUiState()
 }
 
-class HomeViewModel {
+class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel() {
+    var mhsUIState : HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUIState =  HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e:IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteMhs(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
 }
