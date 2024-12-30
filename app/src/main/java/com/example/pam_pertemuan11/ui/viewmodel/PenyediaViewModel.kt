cb63fbdb56d57463fa8cc98fdaeca5c67033d722
DaffaPutra7 <putradika3604@gmail.com>
2024-12-30 14:52:15 +0700

    Menambahkan code pada file PenyediaViewModel.kt berupa initializer untuk DetailViewModel dan UpdateViewModel

--- app/src/main/java/com/example/pam_pertemuan11/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam_pertemuan11/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.pam_pertemuan11.ui.viewmodel
 
 import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -11,6 +12,11 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
         initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer {
+            val savedStateHandle = createSavedStateHandle()
+            DetailViewModel(savedStateHandle, aplikasiMahasiswa().container.mahasiswaRepository)
+        }
+        initializer { UpdateViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
     }
 }
 
