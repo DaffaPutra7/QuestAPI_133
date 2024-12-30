de78328bf835615863dbce7cdc77c56585ce8896
DaffaPutra7 <putradika3604@gmail.com>
2024-12-30 14:53:42 +0700

    Menambahkan code pada file PengelolaHalaman.kt berupa composable untuk DestinasiDetail dan DestinasiUpdate serta melakukan perubahan pada DestinasiHome

--- app/src/main/java/com/example/pam_pertemuan11/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_pertemuan11/ui/navigation/PengelolaHalaman.kt
@@ -3,13 +3,18 @@ package com.example.pam_pertemuan11.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pam_pertemuan11.ui.view.DestinasiDetail
 import com.example.pam_pertemuan11.ui.view.DestinasiEntry
 import com.example.pam_pertemuan11.ui.view.DestinasiHome
+import com.example.pam_pertemuan11.ui.view.DetailView
 import com.example.pam_pertemuan11.ui.view.EntryMhsScreen
 import com.example.pam_pertemuan11.ui.view.HomeScreen
+import com.example.pam_pertemuan11.ui.view.UpdateView
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
@@ -23,7 +28,8 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
-                onDetailClick = {
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
                 }
             )
         }
@@ -36,6 +42,33 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                     }
                 }
             })
+        }
+
+        composable(
+            route = "${DestinasiDetail.route}/{nim}",
+            arguments = listOf(navArgument("nim") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val nim = backStackEntry.arguments?.getString("nim") ?: return@composable
+            DetailView(
+                nim = nim,
+                navigateBack = {
+                    navController.navigateUp()
+                },
+                navController = navController
+            )
+        }
+
+        composable(
+            route = "update_mhs/{nim}",
+            arguments = listOf(navArgument("nim") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val nim = backStackEntry.arguments?.getString("nim") ?: return@composable
+            UpdateView(
+                nim = nim,
+                navigateBack = {
+                    navController.navigateUp()
+                }
+            )
         }
     }
 }
